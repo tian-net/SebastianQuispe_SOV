d178e58b9f205e23c9984d9e1c3452849e1e6601
Tian <freddy.quispe@vallegrande.edu.pe>
2025-02-19 16:08:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,16 +9,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Carga dinámica del video de YouTube
-document.addEventListener('DOMContentLoaded', function() {
-    const youtubeEmbed = document.querySelector('.youtube-embed');
-    youtubeEmbed.innerHTML = `
-        <iframe 
-            width="100%" 
-            height="400" 
-            src="https://www.youtube.com/embed/?listType=user_uploads&list=UCAy8Qkj1AQuqnKiogNdshBA" 
-            frameborder="0" 
-            allowfullscreen>
-        </iframe>
-    `;
-});
